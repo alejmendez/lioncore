da7020b26335a9378d738ece009e4f25b649f52b
Alejandro Mendez <almendez@netred.cl>
2019-07-22 05:16:35 -0400

    Correcciones de seed

--- Modules/User/Database/Seeders/UserDatabaseSeeder.php
+++ Modules/User/Database/Seeders/UserDatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Modules\User\Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
+use Modules\User\Models\User;
+
 class UserDatabaseSeeder extends Seeder
 {
     /**
