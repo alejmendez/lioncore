cfcefd65fbee3047372c9df81e55d78833e41cc3
Alejandro Mendez <almendez@netred.cl>
2019-11-25 22:17:35 -0300

    Se elimina las rutas que no se utilizan y el controlador de auth generico

--- routes/api.php
+++ routes/api.php
@@ -14,38 +14,3 @@ use \App\Laravue\JsonResponse;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::group(['middleware' => 'api'], function () {
-    Route::post('auth/login', 'AuthController@login');
-
-    Route::group(['middleware' => 'auth:api'], function () {
-        Route::get('auth/user', 'AuthController@user');
-        Route::post('auth/logout', 'AuthController@logout');
-    });
-
-    Route::apiResource('users', 'UserController');
-
-    // Fake APIs
-    Route::get('/table/list', function () {
-        $rowsNumber = mt_rand(20, 30);
-        $data = [];
-        for ($rowIndex = 0; $rowIndex < $rowsNumber; $rowIndex++) {
-            $row = [
-                'author' => Faker::randomString(mt_rand(5, 10)),
-                'display_time' => Faker::randomDateTime()->format('Y-m-d H:i:s'),
-                'id' => mt_rand(100000, 100000000),
-                'pageviews' => mt_rand(100, 10000),
-                'status' => Faker::randomInArray(['deleted', 'published', 'draft']),
-                'title' => Faker::randomString(mt_rand(20, 50)),
-            ];
-
-            $data[] = $row;
-        }
-
-        return response()->json(new JsonResponse(['items' => $data]));
-    });
-});
